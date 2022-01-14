266adeb286825c67fdbd04403e5a8efa58036ef8
Mario Serna <mario_sh95@hotmail.com>
2022-01-13 23:28:02 -0600

    add exception handlers

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from logging import exception
 import investpy
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
+from numpy import e
 
 app = Flask(__name__)
 api = Api(app)
@@ -19,49 +21,57 @@ class Welcome(Resource):
 
 class StockList(Resource):
     def get(self):
-        args = parser.parse_args()
-        print(args.list)
-        if args.list != None:
-            stocks = args.list.split(",")
-            print(stocks)
-            frames = []
-            labels = []
-            for stock in stocks:
-                try:
-                    print(stock)
-                    search_result = investpy.search_quotes(
-                        text=stock, products=['stocks'], countries=['peru'], n_results=None)
-                    print(search_result)
-                    if search_result:
-                        data = search_result[0].retrieve_historical_data(
-                            from_date=args.from_date, to_date=args.to_date)
-                        labels.append(stock)
-                        frames.append(data.to_csv())
-                except Exception as e:
-                    print(e)
-                    pass
-            frames.insert(0, ",".join(labels))
-            return frames
+        try:
+            args = parser.parse_args()
+            print(args.list)
+            if args.list != None:
+                stocks = args.list.split(",")
+                print(stocks)
+                frames = []
+                labels = []
+                for stock in stocks:
+                    try:
+                        print(stock)
+                        search_result = investpy.search_quotes(
+                            text=stock, products=['stocks'], countries=['peru'], n_results=None)
+                        print(search_result)
+                        if search_result:
+                            data = search_result[0].retrieve_historical_data(
+                                from_date=args.from_date, to_date=args.to_date)
+                            labels.append(stock)
+                            frames.append(data.to_csv())
+                    except Exception as e:
+                        print(e)
+                        pass
+                frames.insert(0, ",".join(labels))
+                return frames
 
-        search_result = investpy.stocks.get_stocks(country="peru")
-        return search_result.to_csv()
+            search_result = investpy.stocks.get_stocks(country="peru")
+            return search_result.to_csv()
+        except Exception as e:
+            print(e)
+            return ""
 
 
 class Stock(Resource):
     def get(self, id):
-        args = parser.parse_args()
-        print(id)
-        print(args.from_date)
-        search_result = investpy.search_quotes(
-            text=id, products=['stocks'], countries=['peru'], n_results=None)
+        try:
+            args = parser.parse_args()
+            print(id)
+            print(args.from_date)
+            search_result = investpy.search_quotes(
+                text=id, products=['stocks'], countries=['peru'], n_results=None)
 
-        print(search_result)
-        data = search_result[0].retrieve_historical_data(
-            from_date=args.from_date, to_date=args.to_date)
+            print(search_result)
+            data = search_result[0].retrieve_historical_data(
+                from_date=args.from_date, to_date=args.to_date)
 
-        if args.columns:
-            data = data.loc[:, data.columns.isin(args.columns.split(","))]
-        return data.to_csv()
+            if args.columns:
+                data = data.loc[:, data.columns.isin(args.columns.split(","))]
+            return data.to_csv()
+        except Exception as e:
+            print(e)
+            return ""
 
 
 api.add_resource(Welcome, '/')
